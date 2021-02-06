36ac02a6c07791f62c99b495031df03abccd4a02
david-hammond <anotherdavidhammond@gmail.com>
2021-02-06 12:46:31 +1100

    done2

--- src/02-babybernie.R
+++ src/02-babybernie.R
@@ -9,12 +9,12 @@ try(fs::dir_delete(output_image_folder))
 fs::dir_create(output_image_folder)
 
 #Parameters
-screen_size = 1
+screen_size = 2
 num_enter_frames = 5
 df = data.frame(x =c(0.57), y = c(0.48))
 num_frames = 25
 final_bernie = 0.9*screen_size#0.8*log(screen_size) + 1 #2/3 for screen size 1
-penultimate_bernie = 0.7*final_bernie
+penultimate_bernie = 0.2*final_bernie
 #calculate parameters
 scales = 1:num_frames
 hjusts = seq(0,0.2, length.out = length(scales ))
